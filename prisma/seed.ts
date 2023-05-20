46dfe51a3749569cf229d5104c74f8068320b8ef
musa gillani <musagillaniwork@gmail.com>
2023-05-21 01:26:02 +0500

    setup prisma and provision vercel postgres

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import prisma from "../lib/prisma";
+
+async function main() {
+  const response = await Promise.all([
+    prisma.users.upsert({
+      where: { email: "rauchg@vercel.com" },
+      update: {},
+      create: {
+        email: "bme19@example.com",
+        password: "VES19",
+      },
+    }),
+  ]);
+  console.log(response);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
