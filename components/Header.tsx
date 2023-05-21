bf3ba55036402dfa1c7ff5ee7ac87ae28c5ba1f4
musa gillani <musagillaniwork@gmail.com>
2023-05-21 05:36:52 +0500

    added tooltip added logout functionality in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,38 @@
+import { LogOutIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "./ui/button";
+import { signOut } from "next-auth/react";
+
 const Header: React.FC = () => {
   return (
-    <>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        BME-VES
-      </h1>
-      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-        Biomedical Engineering Virtual Education System for Radiology and
-        Nuclear Medicine Devices
-      </h4>
-    </>
+    <div className="flex justify-between">
+      <div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          BME-VES
+        </h1>
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+          Biomedical Engineering Virtual Education System for Radiology and
+          Nuclear Medicine Devices
+        </h4>
+      </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="ghost" onClick={() => signOut()}>
+              <LogOutIcon size={40} className="m-1" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Log out</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   );
 };
 
